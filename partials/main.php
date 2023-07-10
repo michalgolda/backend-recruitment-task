df5d566ef7727bf4922ef78cc55caa2c61c4154d
Michał Gołda <michal.golda@hotmail.com>
2023-07-10 23:36:26 +0200

    feat: initialize user controller and main router

--- partials/main.php
+++ partials/main.php
@@ -1,6 +1,18 @@
 <?php
+require_once __DIR__ . '/settings.php';
+require_once __DIR__ . '/router.php';
 
-// Please add your logic here
+require_once __DIR__ . '/services/user.php';
+require_once __DIR__ . '/repositories/user.php';
+require_once __DIR__ . '/controllers/user.php';
 
-echo "<h1 class='starting-title'>Nice to see you! &#128075;</h1>";
+$userRepository = new UserJSONRepository(SETTINGS['USERS_SOURCE_FILE_PATH']);
+$userService = new UserService($userRepository);
+$userController = new UserController($userService);
 
+$mainRouter = new Router();
+$mainRouter->addRoute('GET', '/', [$userController, 'main'], "index");
+$mainRouter->addRoute('POST', '/add', [$userController, 'addUser'], "addUser");
+$mainRouter->addRoute('POST', '/delete', [$userController, 'deleteUser'], "deleteUser");
+
+$mainRouter->handleRequest($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
